92b612fdda63e616bba863576df8543363ee5879
Ben Hall <b.hall@ucl.ac.uk>
2025-12-11 15:41:16 +0000

    loads model from json into a qn

--- exemplar.py
+++ exemplar.py
@@ -1,5 +1,27 @@
+import json
+
 import clr_loader
 from pythonnet import set_runtime
+
+
+'''
+
+Exemplar illustrating how to work with BMA libraries.
+
+Script 
+-reads "toy model unstable" model
+-performs stability analysis
+-knocks out a gene
+-repeats stability analysis
+
+Current philosophy
+-models are dicts
+-internally these are converted to QNs (don't need to see BMA "model type" directly)
+
+'''
+
+MODEL = "./models/ToyModelUnstable.json"
+DLL = "/home/benjamin/src/pybma/pybma/assemblies/BioCheckAnalyzerMulti.dll"
 
 # Set the runtime to .NET Core before importing clr
 runtime = clr_loader.get_coreclr()
@@ -9,11 +31,63 @@ set_runtime(runtime)
 import clr
 import System
 
-dll_path = "/home/benjamin/src/pybma/pybma/assemblies/BioCheckAnalyzerMulti.dll"
+from System.Collections.Generic import List
+
+
+dll_path = DLL
 clr.AddReference(dll_path)
 
 assembly = System.Reflection.Assembly.LoadFrom(dll_path)
 
+from BioModelAnalyzer import Model
+
+Variable = Model.Variable
+Relationship = Model.Relationship
+
+def loadModel(name):
+    #from a filename, return a BMA model object
+    with open(name, "r") as f:
+        data = json.load(f)
+        print("✓ JSON available!")
+    #load data into model object
+    model = Model()
+    modelData = data['Model']
+    model.Name = modelData['Name']
+
+    #variables_list = List[Variable]()
+    variables = []
+    for var_data in modelData['Variables']:
+        var = Variable()
+        var.Id = var_data.get('Id')
+        var.Name = var_data.get('Name')
+        var.RangeFrom = var_data.get('RangeFrom')
+        var.RangeTo = var_data.get('RangeTo')
+        var.Formula = var_data.get('Formula')
+        variables.append(var)
+    
+    variables_array = System.Array[Variable](variables)
+    model.Variables = variables_array
+
+    relationships = []
+    for rel_data in modelData['Relationships']:
+        rel = Relationship()
+        rel.Id = rel_data.get('Id')
+        #rel.Type = rel_data.get('Type')
+        rel.FromVariable = rel_data.get('FromVariable')
+        rel.ToVariable = rel_data.get('ToVariable')
+        relationships.append(rel)
+    relationships_array = System.Array[Relationship](relationships)
+    model.Relationships = relationships_array
+
+    #turn model into qn
+    marshal = assembly.GetType('Marshal')
+    qn_of_model = marshal.GetMethod("QN_of_Model")
+    args = System.Array[System.Object]([model])
+    result = qn_of_model.Invoke(None, args)
+
+    return(data,result)
+
+'''
 # Find the marshal type
 marshal_type = None
 name = "marshal"
@@ -49,4 +123,4 @@ qn_of_model_method = marshal_type.GetMethod("QN_of_Model")
 
 result = qn_of_model_method.Invoke(None, [your_model])
 
-
+'''
